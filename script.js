23555e4f64c81e29d49e2599e9a55e90528a2745
ormle <101660382+ormle@users.noreply.github.com>
2024-04-18 17:38:32 -0600

    Redesign Portfolio - Implementation of mobile page ONLY (Still need to reconfigure for tablet and desktop)

--- script.js
+++ script.js
@@ -1,23 +1,85 @@
-/*Carousel */
+// /*Carousel */
 
 //Init variables
 //Use . here
-const track = document.querySelector('.carousel__track');
-const slides = Array.from(track.children);
-const nextBtn = document.querySelector('.carouselBtn--right');
-const prevBtn = document.querySelector('.carouselBtn--left');
-const dotsNavs = document.querySelector('.carousel__nav');
-const dots = Array.from(dotsNavs.children);
+const carousels = document.querySelectorAll('.carousel');
+carousels.forEach(carousel => {
+  const track = carousel.querySelector('.carousel__track');
+  const slides = Array.from(track.children);
+  const nextBtn = carousel.querySelector('.carouselBtn--right');
+  const prevBtn = carousel.querySelector('.carouselBtn--left');
+  const dotsNavs = carousel.querySelector('.carousel__nav');
+  const dots = Array.from(dotsNavs.children);
 
-//Get width of slides to find out how much to move 
-const slideWidth = slides[0].getBoundingClientRect().width;
+  //Get width of slides to find out how much to move 
+  const slideWidth = slides[0].getBoundingClientRect().width;
 
-//Arrange slides to be next to each other using absolute positioning
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
+  //Arrange slides to be next to each other using absolute positioning
+  const setSlidePosition = (slide, index) => {
+      slide.style.left = slideWidth * index + 'px';
+  }
+  slides.forEach(setSlidePosition);
+
+  //When click right, move slides to right
+  nextBtn.addEventListener('click', e => {
+    //What slide are we on?
+    //Look through track to find current slide
+    const curSlide = track.querySelector('.cur-slide');
+    const nextSlide = curSlide.nextElementSibling;
+    const curDot = dotsNavs.querySelector('.cur-slide');
+    const nextDot = curDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+    moveToSlide(track, curSlide, nextSlide);
+    updateDots(curDot, nextDot);
+    hideShowArrows(slides, prevBtn, nextBtn, nextIndex);
+  });
+
+  //When click left, move slides to left
+  prevBtn.addEventListener('click', e => {
+    //What slide are we on?
+    //Look through track to find current slide
+    const curSlide = track.querySelector('.cur-slide');
+    const prevSlide = curSlide.previousElementSibling;
+    const curDot = dotsNavs.querySelector('.cur-slide');
+    const prevDot = curDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+    moveToSlide(track, curSlide, prevSlide);
+    updateDots(curDot, prevDot);
+    hideShowArrows(slides, prevBtn, nextBtn, prevIndex);
+  });
+
+  //When click nav indicators, move to the clicked slide
+  dotsNavs.addEventListener('click', e => {
+    //what indicator was clicked
+    const targetDot = e.target.closest('button');//Look for buttons only
+    //null if nav clicked
+    if (!targetDot) return;
+
+    const curSlide = track.querySelector('.cur-slide');
+    const curDot = dotsNavs.querySelector('.cur-slide');
+    //Returns index # of clicked indicator dot
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, curSlide,  targetSlide);
+    updateDots(curDot, targetDot);
+    hideShowArrows(slides, prevBtn, nextBtn, targetIndex);
+  });
+
+});
+
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.scrollY; //Hold previous scroll position
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;// Get current scroll position
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
 }
-slides.forEach(setSlidePosition);
-
 
 //Evals current slide, gets target slide and moves to target slide
 const moveToSlide = (track, curSlide, targetSlide) => {
@@ -48,100 +110,54 @@ const hideShowArrows = (slides, prevBtn, nextBtn, targetIndex) => {
     }
 }
 
-//When click right, move slides to right
-nextBtn.addEventListener('click', e => {
-    //What slide are we on?
-    //Look through track to find current slide
-    const curSlide = track.querySelector('.cur-slide');
-    const nextSlide = curSlide.nextElementSibling;
-    const curDot = dotsNavs.querySelector('.cur-slide');
-    const nextDot = curDot.nextElementSibling;
-    const nextIndex = slides.findIndex(slide => slide === nextSlide);
-    moveToSlide(track, curSlide, nextSlide);
-    updateDots(curDot, nextDot);
-    hideShowArrows(slides, prevBtn, nextBtn, nextIndex);
-});
-
-//When click left, move slides to left
-prevBtn.addEventListener('click', e => {
-    //What slide are we on?
-    //Look through track to find current slide
-    const curSlide = track.querySelector('.cur-slide');
-    const prevSlide = curSlide.previousElementSibling;
-    const curDot = dotsNavs.querySelector('.cur-slide');
-    const prevDot = curDot.previousElementSibling;
-    const prevIndex = slides.findIndex(slide => slide === prevSlide);
-    moveToSlide(track, curSlide, prevSlide);
-    updateDots(curDot, prevDot);
-    hideShowArrows(slides, prevBtn, nextBtn, prevIndex);
-});
-
-//When click nav indicators, move to the clicked slide
-dotsNavs.addEventListener('click', e => {
-    //what indicator was clicked
-    const targetDot = e.target.closest('button');//Look for buttons only
-    //null if nav clicked
-    if (!targetDot) return;
-
-    const curSlide = track.querySelector('.cur-slide');
-    const curDot = dotsNavs.querySelector('.cur-slide');
-    //Returns index # of clicked indicator dot
-    const targetIndex = dots.findIndex(dot => dot === targetDot);
-    const targetSlide = slides[targetIndex];
-
-    moveToSlide(track, curSlide,  targetSlide);
-    updateDots(curDot, targetDot);
-    hideShowArrows(slides, prevBtn, nextBtn, targetIndex);
-});
-
 /* Navigation response */
 
-const nav = document.querySelector('.navigation-container');
-const navBtns = Array.from(nav.children);
-const headers = document.querySelectorAll('.header');
-const welcome = document.querySelector('.welcome');
+// const nav = document.querySelector('.navigation-container');
+// const navBtns = Array.from(nav.children);
+// const headers = document.querySelectorAll('.header');
+// const welcome = document.querySelector('.welcome');
 //console.log(headers);
 
-const updateNav = function(curNavBtn, clickedNav) {
-    curNavBtn.classList.remove('current');
-    clickedNav.classList.add('current');
-}
+// const updateNav = function(curNavBtn, clickedNav) {
+//     curNavBtn.classList.remove('current');
+//     clickedNav.classList.add('current');
+// }
 
-const isInViewPort = function(header) {
-    const rect = header.getBoundingClientRect();
-    return (
-        rect.top >=0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+// const isInViewPort = function(header) {
+//     const rect = header.getBoundingClientRect();
+//     return (
+//         rect.top >=0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
 
 //Respond based on clicks
-nav.addEventListener('click', e => {
-    //Which one was clicked
-    const targetNav = e.target.closest('li');
-    //null if ul clicked
-    if (!targetNav) return;
+// nav.addEventListener('click', e => {
+//     //Which one was clicked
+//     const targetNav = e.target.closest('li');
+//     //null if ul clicked
+//     if (!targetNav) return;
 
-    const curNavBtn = nav.querySelector('.current');
-    //Get index of clicked nav
-    const targetNavIndex = navBtns.findIndex(nav => nav === targetNav);
-    const clickedNav = navBtns[targetNavIndex].firstChild;
+//     const curNavBtn = nav.querySelector('.current');
+//     //Get index of clicked nav
+//     const targetNavIndex = navBtns.findIndex(nav => nav === targetNav);
+//     const clickedNav = navBtns[targetNavIndex].firstChild;
 
-    updateNav(curNavBtn, clickedNav);
-});
+//     updateNav(curNavBtn, clickedNav);
+// });
 
 //Respond based on scroll position
-document.addEventListener('scroll', e  => {
-    //current nav
-    const curNavBtn = nav.querySelector('.current');
-    //Update based on whats in view
-    if (isInViewPort(headers[0])){
-        updateNav(curNavBtn, navBtns[1].firstChild);
-    } else if (isInViewPort(headers[1])){
-        updateNav(curNavBtn, navBtns[2].firstChild);
-    } else if (isInViewPort(headers[2])){
-        updateNav(curNavBtn, navBtns[3].firstChild);
-    }
-});
+// document.addEventListener('scroll', e  => {
+//     //current nav
+//     const curNavBtn = nav.querySelector('.current');
+//     //Update based on whats in view
+//     if (isInViewPort(headers[0])){
+//         updateNav(curNavBtn, navBtns[1].firstChild);
+//     } else if (isInViewPort(headers[1])){
+//         updateNav(curNavBtn, navBtns[2].firstChild);
+//     } else if (isInViewPort(headers[2])){
+//         updateNav(curNavBtn, navBtns[3].firstChild);
+//     }
+// });
